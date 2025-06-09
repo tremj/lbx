951724ba1678d4636bf1ec1e6c1a5217a4947ee8
tremj <tremerjustin@gmail.com>
2025-06-09 00:01:09 -0400

    save config handler

--- cmd/serve.go
+++ cmd/serve.go
@@ -3,12 +3,14 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"github.com/tremj/lbx/internal/api"
+	"github.com/tremj/lbx/internal/storage"
 )
 
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Run the API server",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		storage.InitRedis()
 		return api.StartServer()
 	},
 }
--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,9 +1,30 @@
 package api
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+
+	"github.com/tremj/lbx/internal/core"
+)
 
 func ListConfigsHandler(w http.ResponseWriter, r *http.Request) {}
 
-func SaveConfigHandler(w http.ResponseWriter, r *http.Request) {}
+func SaveConfigHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	name := r.URL.Query().Get("name")
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "invalid body", http.StatusBadRequest)
+		return
+	}
+
+	err = core.SaveConfig(ctx, name, data)
+	if err != nil {
+		http.Error(w, "save failed", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
 
 func DeleteConfigHandler(w http.ResponseWriter, r *http.Request) {}
